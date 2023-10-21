c2d465cb75a92e71d91fda439cc969ecefda7949
imsantosh <santosh76792@gmail.com>
2023-10-21 21:58:02 +0200

    added project structure and with multiple required modules

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
-print('santoh')
-print('smran')
+from setuptools import find_packages, setup
+
+setup(
+    name="helmet",
+    version="0.0.1",
+    author="Santosh",
+    author_email="santosh76792@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
